2e67bdf46ae74b420f3c4877fa415aad39f73a42
MaulanaMadd <djikodotkom@gmail.com>
2024-11-25 08:29:29 +0700

    sudah komit bang

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main (String[] args){
         System.out.println("Hello World");
         System.out.println("kocag kntl cicag");
+        System.out.println("wle wle wle");
+        for (int i = 0; i < 5 ; i++){
+            System.out.println("wiuuu dor");
+        }
     }
 }
