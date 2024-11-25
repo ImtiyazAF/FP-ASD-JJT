2ec6444bb606d7dc46da6a4b94d1de8df1480972
ImtiyazAF <5026231197@student.its.ac.id>
2024-11-25 08:28:48 +0700

    commit agi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args){
         System.out.println("Hello World");
         System.out.println("kocag kntl cicag");
+        System.out.println("Omaga");
     }
 }
